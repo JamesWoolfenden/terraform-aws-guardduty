609c4551e4fc517dc86cbd3012b608954265b8e5
James Woolfenden <james.woolfenden@slalom.com>
2020-02-19 23:28:59 +0000

    wip

--- example/examplea/module.guard.tf
+++ example/examplea/module.guard.tf
@@ -1,5 +1,4 @@
 module "guard" {
-  source              = "../../"
-  common_tags         = var.common_tags
-  log_bucket=var.log_bucket
+  source            = "../../"
+
 }
--- example/examplea/provider.aws.tf
+++ example/examplea/provider.aws.tf
@@ -1,8 +1,6 @@
 provider "aws" {
   region  = "eu-west-1"
-  version = "2.46.0"
+  version = "2.18.0"
 }
 
-provider "template" {
-  version = "2.1"
-}
+
