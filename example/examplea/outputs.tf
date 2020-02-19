18688c69718b648a5a428b55a9d57d1d7caf9013
James Woolfenden <james.woolfenden@gmail.com>
2020-02-19 22:21:02 +0000

    move

---
+++ example/examplea/outputs.tf
@@ -0,0 +1,4 @@
+output "GuardDuty" {
+  value       = module.guard.GuardDuty
+  description = "The GuardDuty detector"
+}
---
+++ example/examplea/provider.aws.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region  = "eu-west-1"
+  version = "2.46.0"
+}
+
+provider "template" {
+  version = "2.1"
+}
